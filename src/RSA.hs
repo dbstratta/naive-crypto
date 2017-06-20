585178e99ab3e595ecc676e75cf30d84d184fb15
Diego Stratta <strattadb@gmail.com>
2017-06-20 00:19:31 -0300

    Add more functions to RSA.hs

--- src/RSA.hs
+++ src/RSA.hs
@@ -1,10 +1,13 @@
 module RSA (rsa) where
 
-type PrivateKey = Integer
+type PrivateKey = (Integer, Integer)
 type PublicKey = (Integer,Integer)
 
 type Message = String
+type EncodeMessage = Integer
 type EncryptedMessage = Integer
+type Sign = Integer
+type SignedMessage = (Sign,Message)
 
 data KeyPair = KeyPair (PrivateKey, PublicKey) deriving (Show, Eq)
 
@@ -14,24 +17,37 @@ phiForPrimes = foldr (*) 1 . map (flip (-) 1)
 generatePublicKey :: Integer -> Integer -> PublicKey
 generatePublicKey n e = (n, e)
 
-generatePrivateKey :: Integer -> Integer -> PrivateKey
-generatePrivateKey p = inverse
+generatePrivateKey :: Integer -> Integer -> Integer -> PrivateKey
+generatePrivateKey n p e = (inverse e, n)
   where
     inverse e = head [d | d <- [1..p - 1], (d * e) `mod` p == 1]
 
 generateKeyPair :: Integer -> Integer -> KeyPair
 generateKeyPair p q = KeyPair (privateKey, publicKey)
   where
-    phiPQ = phiForPrimes [p, q]
-    e = last [m | m <- [1..phiPQ - 1], gcd m phiPQ  == 1]
-    privateKey = generatePrivateKey phiPQ e :: PrivateKey
+    n = p * q
+    phiN = phiForPrimes [p, q]
+    e = last [m | m <- [1..phiN - 1], gcd m phiN  == 1]
+    privateKey = generatePrivateKey n phiN e :: PrivateKey
     publicKey = let n = p * q in generatePublicKey n e :: PublicKey
 
 -- TODO: Implement this.
-encodeMessage :: Message -> Integer
+encodeMessage :: Message -> EncodeMessage
 encodeMessage = id
 
-encryptWithPublicKey :: PublicKey -> Message -> EncryptedMessage
-encryptWithPublicKey (n, e) m = encodedMessage ^ e `mod` n
+-- TODO: Implement this.
+decodeMessage :: EncodeMessage -> Message
+decodeMessage = id
+
+encrypt :: PublicKey -> Message -> EncryptedMessage
+encrypt (n, e) m = encodedMessage ^ e `mod` n
+  where
+    encodedMessage = encodeMessage m
+
+decrypt :: PrivateKey -> EncryptedMessage -> Message
+decrypt (d, n) encryptedMessage = encryptedMessage
+
+sign :: PrivateKey -> Message -> EncryptedMessage
+sign (d, n) m = encodedMessage ^ d `mod` n
   where
     encodedMessage = encodeMessage m
